9781606e22e6fd83c7954d050bea9fec71a3ac19
Nandan Varma <137528244+NandanUnv@users.noreply.github.com>
2024-08-04 22:42:59 +0530

    Update code.java

--- Question 1/code.java
+++ Question 1/code.java
@@ -1,1 +1,40 @@
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+      
+      Scanner sc = new Scanner(System.in);
+      
+      int n = sc.nextInt();
+      
+      int date[] = new int[n];
+      
+      int mon[] = new int[n];
+      
+      int year[] = new int[n];
+      
+      for (int i=0; i<n; i++){
+        date[i] = sc.nextInt();
+        mon[i] = sc.nextInt();
+        year[i] = sc.nextInt();
+      } 
+      
+      
+      int s_year[] = new int[n];
+      
+      for(int h=0; h<n; h++){
+        s_year[h] = year[h];
+      }
+      
+      Arrays.sort(s_year);
+ 
+      for(int b=0; b<s_year.length; b++){
+        for(int j=0; j<n; j++){
+          if(s_year[b]==year[j]){
+            System.out.println(date[j]+" "+mon[j]+" "+year[j]);
+            break;
+          }
+        }
+      }
+  }
+}
